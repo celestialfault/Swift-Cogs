4cc6bfd2bd3a28c17511bfde82253e2a547bb7e0
odinair <odinair@odinair.xyz>
2018-02-05 23:19:05 -0700

    [odinair_libs] v0.2.0
    + Added formatting.changed
    * Refactored menus.react_menu

--- odinair_libs/formatting.py
+++ odinair_libs/formatting.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Any, Dict, Union
 
 
 def td_format(td_object: timedelta, short_format: bool = False, as_string: bool = True) -> str:
@@ -30,13 +30,15 @@ def td_format(td_object: timedelta, short_format: bool = False, as_string: bool 
             else:
                 strings.append("%s %ss" % (period_value, period_name))
 
-    return ", ".join(strings) if as_string is True else strings
+    return (", " if not short_format else "").join(strings) if as_string is True else strings
 
 
-def difference(list1: Iterable, list2: Iterable, *, check_val: bool = False) -> Tuple[list, list]:
+def difference(list1: Iterable, list2: Iterable, *, check_val: bool = False, dict_values: bool = False)\
+        -> Tuple[Union[Iterable, Dict], Union[Iterable, Dict]]:
     """Returns a tuple of lists based on the Iterable items passed in
 
-    If check_val is True, this assumes the lists contain tuple-like items, and checks for True-ish items"""
+    If check_val is True, this assumes the lists contain tuple-like items, and checks for True-ish items
+    """
     if check_val:
         # Only include items that evaluate to True
         list1 = [x for x, val in list1 if val]
@@ -44,7 +46,22 @@ def difference(list1: Iterable, list2: Iterable, *, check_val: bool = False) -> 
 
     added = [x for x in list2 if x not in list1]
     removed = [x for x in list1 if x not in list2]
+
+    if dict_values:
+        added = {x: list2[x] for x in added}
+        removed = {x: list1[x] for x in removed}
     return added, removed
+
+
+def changed(before: Dict[Any, Any], after: Dict[Any, Any]) -> Tuple[Dict, Dict, Dict, Dict]:
+    """Returns a list of added, removed, and changed items from two dict-like objects
+
+    This assumes that the first key of an object is a unique key, and the second key is an item that can be changed
+    """
+    added, removed = difference(before, after, dict_values=True)
+    changed_before = {x: before[x] for x in after if x in after and after[x] != before[x]}
+    changed_after = {x: after[x] for x in after if x in before and before[x] != after[x]}
+    return added, changed_before, changed_after, removed
 
 
 def normalize(text, *, title_case: bool = True, **kwargs):
--- odinair_libs/menus.py
+++ odinair_libs/menus.py
@@ -1,54 +1,233 @@
-import asyncio
-from typing import Union
+from asyncio import TimeoutError
+
+from collections import namedtuple
+from enum import Enum
+from typing import Union, Optional, Any, Dict
 
 import discord
 from redbot.core import RedContext
-from redbot.core.bot import Red
 
 
-async def confirm(bot: Red, ctx: RedContext, message: str, timeout: Union[float, int] = 30.0,
-                  colour: discord.Colour = discord.Colour.blurple()):
-    """Prompt a user for confirmation"""
-    actions = {"\N{WHITE HEAVY CHECK MARK}": "confirm", "\N{REGIONAL INDICATOR SYMBOL LETTER X}": "cancel"}
-    message = ("{}\n\n"
-               "**Click \N{WHITE HEAVY CHECK MARK} to confirm or "
-               "\N{REGIONAL INDICATOR SYMBOL LETTER X} to cancel**"
-               "".format(message))
-    return await react_menu(bot, ctx, message, actions, default="\N{REGIONAL INDICATOR SYMBOL LETTER X}",
-                            timeout=timeout, colour=colour) == "confirm"
+class PostMenuAction(Enum):
+    """The action to take after a menu action or timeout"""
+    DELETE = "delete"
+    CLEAR_REACTIONS = "clear_react"
+    REMOVE_REACTION = "remove_react"
+    NONE = "none"
 
 
-async def react_menu(bot: Red, ctx: RedContext, message: str, actions: dict, member: discord.Member = None,
-                     default: str = None, colour: discord.Colour = discord.Colour.blurple(),
-                     timeout: Union[float, int] = 30.0, delete_msg: bool = True):
+class MenuResult:
+    """A result from a call to `react_menu`
+
+    Attributes
+    -----------
+
+        action: Any
+
+            The action chosen by the user, or the default action
+
+        message: Optional[discord.Message]
+
+            The sent message, or None if the menu deleted the message afterwards
+
+        timed_out: bool
+
+            If the menu timed out and returned the default value, this value will be True. Otherwise, False
+    """
+    def __init__(self, action: Any, message: Optional[discord.Message], timed_out: bool):
+        self.action = action
+        self.message = message
+        self.timed_out = timed_out
+
+
+async def confirm(ctx: RedContext, message: str, timeout: float = 30.0,
+                  colour: discord.Colour = discord.Colour.blurple(), default: bool = False, **kwargs) -> bool:
+    """Prompt a user for confirmation on an action
+
+    Parameters
+    -----
+
+        ctx: RedContext
+
+            The Red context object
+
+        message: str
+
+            The message to display on the confirmation prompt
+
+        timeout: float
+
+            How long to wait in seconds before timing out with the default value
+
+            Default value: 30.0
+
+        colour: discord.Colour
+
+            The colour to use for the message embed
+
+            Default value: `discord.Colour.blurple()`
+
+        default: bool
+
+            The default return value if the prompt times out
+
+            Default value: `False`
+
+    Returns
+    -----
+
+        True
+
+            Returned if the user confirmed the action or if the timeout was reached and the default was True
+
+        False
+
+            Returned if the user declined the action or if the timeout was reached and the default was False
+    """
+    message = (
+        "{}\n\n"
+        "**Click \N{WHITE HEAVY CHECK MARK} to confirm or \N{REGIONAL INDICATOR SYMBOL LETTER X} to cancel**"
+        "".format(message)
+    )
+    result = await react_menu(ctx, {True: "\N{WHITE HEAVY CHECK MARK}",
+                                          False: "\N{REGIONAL INDICATOR SYMBOL LETTER X}"},
+                                    embed=discord.Embed(description=message, colour=colour),
+                                    default=default, timeout=timeout, post_action=PostMenuAction.DELETE, **kwargs)
+    return result.action
+
+
+async def react_menu(ctx: RedContext,
+                     actions: Dict[Any, Union[discord.Emoji, discord.Reaction, str]],
+                     default: Any = None, content: str = None, embed: discord.Embed = None,
+                     message: discord.Message = None, member: discord.Member = None, timeout: float = 30.0,
+                     post_action: PostMenuAction = PostMenuAction.REMOVE_REACTION) -> MenuResult:
+    """Create a reaction menu
+
+    Parameters
+    -----------
+
+        ctx: RedContext
+
+            The Red context object
+
+        actions: Dict[Any, Union[discord.Emoji, discord.Reaction, str]
+
+            A dict of actions, similar to {action: emoji, ...}
+            There can only be up to 15 different actions
+
+        default: Any
+
+            The default action.
+
+            Default value: `None`
+
+        content: str
+
+            The message content when sending a message if `message` is not specified
+
+            Default value: `None`
+
+        embed: discord.Embed
+
+            A message embed to use when sending a message if `message` is not specified
+
+            Default value: `None`
+
+        message: discord.Message
+
+            A message to re-use. If this is present, all the reactions are expected to be present.
+            This is best used with the remove reaction action if this is a message being re-used from prior
+            react menu calls.
+
+            Default value: `None`
+
+        member: discord.Member
+
+            The member to listen to reactions from.
+
+            This is set to the value of `ctx.author` if `None` is given.
+
+            Default value: `None`
+
+        timeout: float
+
+            How long to wait for a reaction before timing out.
+
+            Default value: `30.0`
+
+        post_action: PostMenuAction
+
+            The action to take when a user chooses a reaction.
+            If the menu times out, this will default to clearing reactions if this is not `PostMenuAction.DELETE`
+
+            Default value: `PostMenuAction.REMOVE_REACTION`
+
+    Returns
+    --------
+
+        MenuResult
+
+            The resulting action chosen can be accessed with `<MenuResult>.action`
+
+    Raises
+    -------
+
+        discord.Forbidden
+
+            If the bot is not allowed to send messages in the context channel
+
+        discord.HTTPException
+
+            A generic HTTP exception
+
+        ValueError
+
+            Raised if more than 15 different actions are given
+
+        RuntimeError
+
+            Raised if none of either message, embed or content attributes are given
+    """
     if len(actions.keys()) > 15:
-        raise RuntimeError("You can only have at most 15 different actions")
+        raise ValueError("You can only have at most up to 15 different actions")
+    if not any([message, embed, content]):
+        raise RuntimeError("Expected message, embed or content attributes, received none")
+
     member = member or ctx.author
-    embed = discord.Embed(description=message, colour=colour)
-    react_msg = await ctx.send(embed=embed)
+    bot = ctx.bot
+    emojis = [actions[x] for x in actions]
+    actions = [x for x in actions]
 
-    for emoji in actions.keys():
-        await react_msg.add_reaction(emoji)
+    if not message:
+        message = await ctx.send(content=content, embed=embed)
 
-    def check(_react, _user):
-        msg = _react.message
-        return msg.id == react_msg.id and _user.id == member.id and str(_react.emoji) in actions.keys()
+        for emoji in emojis:
+            await message.add_reaction(emoji)
 
-    _ret_val = None
+    def check(react_, user_):
+        msg = react_.message
+        return msg.id == message.id and user_.id == member.id and str(react_.emoji) in emojis
 
+    reaction = namedtuple("Reaction", "emoji")(emoji=None)
+    ret_val = default
+    timed_out = False
     try:
-        reaction, emoji = await bot.wait_for("reaction_add", check=check, timeout=timeout)
-    except asyncio.TimeoutError:
-        _ret_val = actions.get(default, None)
+        reaction, _ = await bot.wait_for("reaction_add", check=check, timeout=timeout)
+    except TimeoutError:
+        timed_out = True
     else:
-        _ret_val = actions.get(str(reaction.emoji), actions.get(default, None))
+        if reaction.emoji in emojis:
+            ret_val = actions[emojis.index(reaction.emoji)]
     finally:
-        if delete_msg:
-            await react_msg.delete()
-        else:
-            if getattr(ctx, "guild", None) and ctx.channel.permissions_for(ctx.guild.me).manage_messages:
-                await react_msg.clear_reactions()
-        return _ret_val
+        if post_action == PostMenuAction.DELETE:
+            await message.delete()
+        elif getattr(ctx, "guild", None) and ctx.channel.permissions_for(ctx.guild.me).manage_messages:
+            if post_action == PostMenuAction.CLEAR_REACTIONS or timed_out:
+                await message.clear_reactions()
+            elif post_action == PostMenuAction.REMOVE_REACTION:
+                await message.remove_reaction(reaction.emoji, member)
+        message = message if post_action != PostMenuAction.DELETE else None
+        return MenuResult(ret_val, message, timed_out)
 
 
 async def cmd_help(ctx: RedContext, cmd: str) -> None:
