6eb9c5b3a3a50bfd735e1ed0bba2b4dbb9b34978
odinair <odinair@odinair.xyz>
2018-02-23 07:06:43 -0700

    [Starboard] Fix config

--- starboard/__init__.py
+++ starboard/__init__.py
@@ -6,11 +6,11 @@ from starboard.classes.starboardbase import StarboardBase
 
 async def setup(bot: Red):
     if 'OdinairLibs' not in bot.cogs:
-        spec = await bot.cog_mgr.find_cog('odinair_libs')
-        await bot.load_extension(spec)
+        await bot.load_extension(await bot.cog_mgr.find_cog('odinair_libs'))
     from .starboard import Starboard
     from .classes.starboardbase import setup as setup_base
-    config = Config.get_conf(Starboard, identifier=45351212589, force_registration=True)
+    from odinair_libs.config import fix_config_fuckup
+    config = Config.get_conf(None, identifier=45351212589, force_registration=True, cog_name="Starboard")
     config.register_guild(**{
         "messages": [],
         "blocks": [],
@@ -19,5 +19,6 @@ async def setup(bot: Red):
         "min_stars": 1,
         "respect_requirerole": True
     })
+    await fix_config_fuckup(config, identifier=45351212589, bot=bot)
     setup_base(bot, config)
     bot.add_cog(Starboard(bot, config))
