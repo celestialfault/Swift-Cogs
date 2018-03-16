a2cd0c446ecd3e1ce051e66aa6c64cae048f86ad
odinair <odinair@odinair.xyz>
2018-03-16 05:46:45 -0600

    [UInfo] Initial pass on 18n

--- uinfo/uinfo.py
+++ uinfo/uinfo.py
@@ -4,8 +4,11 @@ from discord.ext import commands
 from redbot.core import RedContext
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import escape, bold
+from redbot.core.i18n import CogI18n
 
-from odinair_libs.formatting import td_format, normalize
+from odinair_libs.formatting import td_format
+
+_ = CogI18n("UInfo", __file__)
 
 
 class UInfo:
@@ -23,7 +26,7 @@ class UInfo:
         if len(items) >= 3:
             last = items.pop()
             joined = ", ".join(items)
-            return f"{joined} and {last}"
+            return _("{} and {}").format(joined, last)
         else:
             return ", ".join(items)
 
@@ -33,16 +36,16 @@ class UInfo:
         msgs = []
 
         if await self.bot.is_owner(member):
-            msgs.append("\N{HAMMER AND WRENCH} **Bot Owner**")
+            msgs.append(_("\N{HAMMER AND WRENCH} **Bot Owner**"))
 
         if member.guild.owner.id == member.id:
-            msgs.append("\N{KEY} Guild Owner")
+            msgs.append(_("\N{KEY} Guild Owner"))
         elif await self.bot.is_admin(member):
-            msgs.append("\N{HAMMER} Server Administrator")
+            msgs.append(_("\N{HAMMER} Server Administrator"))
         elif await self.bot.is_mod(member):
-            msgs.append("\N{SHIELD} Server Moderator")
+            msgs.append(_("\N{SHIELD} Server Moderator"))
         else:
-            msgs.append("\N{BUSTS IN SILHOUETTE} Server Member")
+            msgs.append(_("\N{BUSTS IN SILHOUETTE} Server Member"))
 
         return "\n".join(msgs)
 
@@ -54,22 +57,34 @@ class UInfo:
             pass
 
         elif member.activity.type == discord.ActivityType.playing:
-            game_type = "\N{VIDEO GAME} Playing"
+            game_type = _("\N{VIDEO GAME} Playing")
 
         elif member.activity.type == discord.ActivityType.streaming:
-            game_type = "\N{VIDEO CAMERA} Streaming"
+            game_type = _("\N{VIDEO CAMERA} Streaming")
             game = bold(f"[{member.activity}]({member.activity.url})")
 
         elif member.activity.type == discord.ActivityType.listening:
-            game_type = "\N{MUSICAL NOTE} Listening to"
+            game_type = _("\N{MUSICAL NOTE} Listening to")
             if isinstance(member.activity, discord.Spotify):
-                game = f"{self.plural(*[bold(x) for x in member.activity.artists])} \N{EM DASH} " \
-                       f"**{member.activity.title}** on **Spotify**"
+                game = _("{} \N{EM DASH} **{}** on **Spotify**").format(
+                    self.plural(*[bold(x) for x in member.activity.artists]), member.activity.title)
 
         elif member.activity.type == discord.ActivityType.watching:
-            game_type = "\N{FILM PROJECTOR} Watching"
+            game_type = _("\N{FILM PROJECTOR} Watching")
 
         return game, game_type
+
+    @staticmethod
+    def get_status(member: discord.Member):
+        """This is a terrible way to handle i18n with status strings."""
+        if member.status == discord.Status.online:
+            return _("Online")
+        elif member.status == discord.Status.idle:
+            return _("Idle")
+        elif member.status == discord.Status.dnd:
+            return _("Do Not Disturb")
+        else:
+            return _("Offline")
 
     @commands.command(aliases=["uinfo", "whois"])
     @commands.guild_only()
@@ -86,28 +101,28 @@ class UInfo:
             colour = discord.Embed.Empty
 
         activity, activity_type = self.get_activity(user)
-        status = normalize(str(user.status), dnd="do not disturb")
+        status = self.get_status(user)
         description = f"\N{EARTH GLOBE AMERICAS} {status}"
 
         if activity_type is not None:
             description += f"\n{activity_type} {activity}"
         if user.nick:
-            description += f"\n\N{LABEL} Nicknamed as {bold(escape(user.nick, formatting=True))}"
+            description += _("\n\N{LABEL} Nicknamed as {}").format(bold(escape(user.nick, formatting=True)))
 
         embed = discord.Embed(title=str(user), colour=colour, description=description)
         embed.set_thumbnail(url=user.avatar_url)
-        embed.set_footer(text=f"Member #{member_number} | User ID: {user.id}")
+        embed.set_footer(text=_("Member #{} | User ID: {}").format(member_number, user.id))
 
         bot_roles = await self.get_bot_role(user)
         if bot_roles is not None:
-            embed.add_field(name="Bot Roles", value=bot_roles, inline=False)
+            embed.add_field(name=_("Bot Roles"), value=bot_roles, inline=False)
 
         roles = reversed([escape(x.name, formatting=True) for x in user.roles if not x.is_default()])
         if roles:
-            embed.add_field(name="Guild Roles", value=", ".join(roles or ["None"]), inline=False)
+            embed.add_field(name=_("Guild Roles"), value=", ".join(roles or ["None"]), inline=False)
 
-        embed.add_field(name="Joined Discord", value=since_created, inline=False)
-        embed.add_field(name="Joined Guild", value=since_joined, inline=False)
+        embed.add_field(name=_("Joined Discord"), value=since_created, inline=False)
+        embed.add_field(name=_("Joined Guild"), value=since_joined, inline=False)
         await ctx.send(embed=embed)
 
     @commands.command(name="avatar")
@@ -115,6 +130,6 @@ class UInfo:
     async def avatar(self, ctx: RedContext, *, user: discord.Member = None):
         """Get the avatar of yourself or a specified user"""
         user = ctx.author if user is None else user
-        embed = discord.Embed(colour=user.colour, title="{0!s}'s avatar".format(user))
+        embed = discord.Embed(colour=user.colour, title=_("{}'s avatar").format(str(user)))
         embed.set_image(url=user.avatar_url_as(static_format="png"))
         await ctx.send(embed=embed)
