db5ec9a70a1cbc24ec64dcc377a310dca6c8d9b4
odinair <odinair@odinair.xyz>
2018-03-16 05:50:17 -0600

    [MiscTools] Initial pass on i18n

--- misctools/misctools.py
+++ misctools/misctools.py
@@ -4,17 +4,23 @@ import discord
 from discord.ext import commands
 
 from redbot.core.bot import Red, RedContext
-from redbot.core.utils.chat_formatting import warning, pagify
+from redbot.core.utils.chat_formatting import warning, pagify, box
+from redbot.core.i18n import CogI18n
 
 import unicodedata
 
 from odinair_libs.formatting import td_format, get_source
+
+_ = CogI18n("MiscTools", __file__)
 
 
 class MiscTools:
     """Various quick & dirty utilities
     Mostly useful when making cogs, and/or for advanced server administration use.
     """
+
+    __author__ = "odinair <odinair@odinair.xyz>"
+    __version__ = "0.1.0"
 
     def __init__(self, bot: Red):
         self.bot = bot
@@ -24,21 +30,27 @@ class MiscTools:
         """Get the source for a command or sub command"""
         command = self.bot.get_command(command_name)
         if command is None:
-            await ctx.send(warning("That command doesn't exist"))
+            await ctx.send(warning(_("That command doesn't exist")))
             return
         await ctx.send_interactive(pagify(get_source(command.callback), shorten_by=10), box_lang="py")
 
     @commands.command()
     async def charinfo(self, ctx: RedContext, *, characters: str):
-        """Get the unicode name for characters"""
-        if len(characters) > 25:
-            await ctx.send(warning("You can only pass up 25 characters at once"))
-            return
-        await ctx.send("\n".join(map(lambda c: "{0} \N{EM DASH} {1}".format(c, unicodedata.name(c, "Name not found")),
-                                     characters)))
+        """Get the unicode name for characters
 
-    @commands.command(name="pingtime", aliases=["pingt"])
-    async def ping(self, ctx: RedContext):
+        Up to 25 characters can be given at one time
+        """
+        if len(characters) > 25:
+            await ctx.send_help()
+            return
+
+        def convert(c):
+            return f"{c} \N{EM DASH} {unicodedata.name(c, 'Name not found')}"
+
+        await ctx.send("\n".join(map(convert, characters)))
+
+    @commands.command(aliases=["pingt"])
+    async def pingtime(self, ctx: RedContext):
         """Get the time it takes the bot to respond to a command
 
         This is by no means fully accurate, and should be treated similarly to rough estimate
@@ -46,18 +58,17 @@ class MiscTools:
         Time to command execution means how long it took for the bot to receive the command message
         and execute the command
         """
-        to_execution = td_format(datetime.utcnow() - ctx.message.created_at, milliseconds=True, short_format=True)
+        time_to_execution = td_format(datetime.utcnow() - ctx.message.created_at, milliseconds=True, short_format=True)
         now = datetime.utcnow()
         await ctx.trigger_typing()
         time_to_typing = td_format(datetime.utcnow() - now, milliseconds=True, short_format=True)
         full_round_trip = td_format(datetime.utcnow() - ctx.message.created_at, milliseconds=True, short_format=True)
-        await ctx.send("\N{TABLE TENNIS PADDLE AND BALL} Pong!"
-                       "\nTime to command execution: {execution}"
-                       "\nTyping indicator: {typing}"
-                       "\n\nFull round trip: {rt}"
-                       "".format(execution=to_execution, typing=time_to_typing, rt=full_round_trip))
+        await ctx.send(_("\N{TABLE TENNIS PADDLE AND BALL} Pong!\n"
+                         "Time to command execution: {}\n"
+                         "Typing indicator: {}\n\n"
+                         "Full round trip: {}").format(time_to_execution, time_to_typing, full_round_trip))
 
-    @commands.command()
+    @commands.command(aliases=["snowflaketime"])
     async def snowflake(self, ctx: RedContext, *snowflakes: int):
         """Get the time that one or more snowflake IDs were created at"""
         if not snowflakes:
@@ -66,8 +77,8 @@ class MiscTools:
         strs = []
         for snowflake in snowflakes:
             snowflake_time = discord.utils.snowflake_time(snowflake)
-            strs.append("{0}: `{1}` ({2} ago)".format(snowflake, snowflake_time,
-                                                      td_format(datetime.utcnow() - snowflake_time)))
+            strs.append(f"{snowflake}: `{snowflake_time.strftime('%A %B %d, %Y at %X UTC')}` \N{EM DASH} "
+                        f"{td_format(snowflake_time - datetime.utcnow(), append_str=True)}")
         await ctx.send_interactive(pagify("\n".join(strs)))
 
     @commands.command(hidden=True)
@@ -75,41 +86,21 @@ class MiscTools:
         """A very simple example command that uses ReactMenu
 
         This isn't a proper command (unless you like being asked for a number between one and three),
-        and is meant as an example for my library cog's `ReactMenu` function.
-        You can retrieve the source for this with `[p]rtfs test_menu`.
+        and is meant as an example for my library cog's `ReactMenu` class.
 
         The aforementioned library cog can be found as `odinair_libs`
         in my cog repository: https://github.com/notodinair/Red-Cogs/tree/v3
         """
-        from odinair_libs.menus import PostMenuAction, ReactMenu
-        from odinair_libs.formatting import attempt_emoji
+        from odinair_libs.menus import ReactMenu, PostMenuAction
         # This command is meant as a fairly basic example usage of ReactMenu,
         # with comments to explain most features used.
         # There's a *lot* more functionality that isn't covered here.
-        # If you'd like to use this in an external cog, feel free to adapt this to your own use case
-        # For an example on pagination, see the `logset info` command in my Logs cog
-        # For more information on the following fields used, please check the docs for the ReactMenu function
-
-        # The following function attempts to retrieve a guild emoji by ID and / or name, with a fallback unicode emoji
-        # In this instance, the fallback unicode emoji is simply an empty string, however you can for example
-        # pass in a unicode emoji as a fallback (such as '\N{CROSS MARK}')
-        # The emoji used below: https://cdn.discordapp.com/emojis/379046291386400769.png
-        # Example usages can include guild-configurable emojis for actions
-        null_tick = attempt_emoji(
-            bot=self.bot,
-            # an emoji id to attempt to retrieve, this can be useful for cogs designed for a specific bot
-            # otherwise, if guilds have their own copy of the same or a variation of a specific emoji,
-            # emoji_name may be better suited
-            emoji_id=379046291386400769,
-            # a case sensitive emoji name to try to resolve
-            # this is ignored if an exact emoji is found from the emoji_id field
-            emoji_name="NullTick",
-            # fallback is used if neither emoji_id nor emoji_name finds an emoji
-            fallback="")
+        # If you'd like to use this in an external cog, feel free to adapt this to your own use case.
+        # Implementing i18n support in this example is left as an exercise for the reader.
 
         # This is a dict of actions in the form of { action: emoji }
         # Note that the default value (passed as the `default` keyword argument when creating a ReactMenu)
-        # does *not* need to be in the actions dict
+        # does not need to be in the actions dict
         # Accepted emoji types are unicode emojis (such as below), or `discord.Emoji` items
         # The action is returned as an attribute named `action` in a MenuResult class, in the form of either
         # a key in the actions dict you passed when creating the ReactMenu, or the value of `default`
@@ -118,10 +109,8 @@ class MiscTools:
             "Two": "\N{DIGIT TWO}\N{COMBINING ENCLOSING KEYCAP}",
             "Three": "\N{DIGIT THREE}\N{COMBINING ENCLOSING KEYCAP}"
         }
-        if null_tick != "":
-            actions["Null Tick"] = null_tick
-        menu = ReactMenu(ctx, actions, content="Choose a number", post_action=PostMenuAction.DELETE, default="Default")
-        # Prompt the user for a choice
-        result = await menu.prompt()
-        # Send the result in chat
-        await ctx.send("Result:\n```\n{0!r}\n```".format(result))
+        menu = ReactMenu(ctx=ctx, actions=actions, content="Choose a number", post_action=PostMenuAction.DELETE,
+                         default="Default")
+        async with menu as result:
+            # Send the result in chat
+            await ctx.send(f"Result:\n{box(repr(result), lang='py')}")
