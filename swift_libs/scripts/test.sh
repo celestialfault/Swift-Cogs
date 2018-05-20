232665739090afb75f320d6e742e27c625ac389f
odinair <odinair@odinair.xyz>
2018-05-20 04:31:06 -0600

    are you fucking kidding me

--- swift_libs/scripts/test.sh
+++ swift_libs/scripts/test.sh
@@ -5,5 +5,12 @@ set -e
 export cogs=(cogwhitelist logs misctools quotes requirerole rndactivity rolemention starboard timedmute timedrole uinfo swift_libs)
 
 python3 -m compileall ${cogs[*]}
-flake8 ${cogs[*]} --max-line-length 100 --show-source --statistics --ignore F401,W503
+# explanations for the following ignored checks:
+# - F401:
+#   Unused imports, since some <3.6 style type hints are still used in various places
+# - W503:
+#   Black moves binary operators onto their own lines, which is incompatible with this check
+# - E203:
+#   Incorrectly checks slices (such as `<list>[1 : 2]`)
+flake8 ${cogs[*]} --max-line-length 100 --show-source --statistics --ignore F401,W503,E203
 black --check -l 100 ${cogs[*]}
