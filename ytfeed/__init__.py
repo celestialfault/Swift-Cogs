f0dcd1f2827c77227a2a772eb0544191c2535694
odinair <odinair@odinair.xyz>
2018-05-25 14:16:25 -0600

    remove ytfeed since it's a terrible idea, use a dedicated rss cog instead

--- ytfeed/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ytfeed import YTFeed
-
-
-def setup(bot):
-    bot.add_cog(YTFeed(bot))
--- ytfeed/config.py
+++
@@ -1,8 +0,0 @@
-from redbot.core import Config
-from redbot.core.i18n import Translator
-
-i18n = Translator("YTFeeds", __file__)
-
-config = Config.get_conf(None, cog_name="YTFeeds", identifier=2342355123)
-config.register_custom("FEED", last_video=0, channels=[])
-config.register_guild(message=i18n("**{channel.name}** has uploaded a new video!"))
