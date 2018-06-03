47747ccc33617dcaa3851ff12c6f95aee675d1e6
odinair <odinair@odinair.xyz>
2018-06-03 17:35:52 -0600

    move repo to gitlab

--- swift_libs/__init__.py
+++ swift_libs/__init__.py
@@ -11,3 +11,12 @@ from .i18n import LazyString, LazyTranslator, to_lazy_translator, fi18n  # noqa
 from .menus import *  # noqa
 from .menus_legacy import *  # noqa
 from .time import *  # noqa
+
+__import__("logging").getLogger("red.swift_libs").warn(
+    "Hi! It seems you're using my GitHub cog repository.\n"
+    "I've since moved these to GitLab, and the cogs from this repository"
+    " won't be updated anymore.\n\n"
+    "If you'd like to continue getting updates, please update your bot "
+    "to use this git repository instead: https://gitlab.com/odinair/Swift-Cogs\n\n"
+    "Thanks!~ \N{PURPLE HEART}\n  ~ odinair"
+)
